5316fd3ccb55a00f7bd214e4b6edb7b6c704a960
jstarling1 <jennstarling@gmail.com>
2016-10-01 11:00:01 -0500

    Exercise 4 Updates

---
+++ bigdata:Exercise 04:SGD_Adagrad_URL_Eigen.R
@@ -0,0 +1,115 @@
+### SDS 385 - Exercises 04 - Part B
+#This code implements stochastic gradient descent
+#using adagrad.  Reads in the malicious URL data.
+
+#Jennifer Starling
+#18 September 2016
+
+rm(list=ls())	#Cleans workspace.
+
+library(microbenchmark)
+library(Rcpp)
+library(RcppEigen)
+library(Matrix)
+library(permute)
+
+#Load C++ file.
+sourceCpp(file='/Users/jennstarling/UTAustin/2016_Fall_SDS 385_Big_Data/Exercise 04 R Code/stoch_grad_desc_logit_binom_Eigen.cpp')
+
+#------------------------------------------------------------------
+#Read in small data set.
+
+#Read in code.
+wdbc = read.csv('/Users/jennstarling/UTAustin/2016_Fall_SDS 385_Big_Data/Course Data/wdbc.csv', header=FALSE)
+y = wdbc[,2]
+
+#Convert y values to 1/0's.
+Y = rep(0,length(y)); Y[y=='M']=1
+X = as.matrix(wdbc[,-c(1,2)])
+
+#Select features to keep, and scale features.
+scrub = which(1:ncol(X) %% 3 == 0)
+scrub = 11:30
+X = X[,-scrub]
+X <- scale(X) #Normalize design matrix features.
+X = cbind(rep(1,nrow(X)),X)
+X = Matrix(X,sparse=T)
+
+Xt = t(X)
+
+#Set up vector of sample sizes.  (All 1 for wdbc data.)
+m <- rep(1,nrow(X))	
+
+#------------------------------------------------------------------
+#Read in large sparse URL data set.
+
+#READ IN DATA:
+Xt_URL <- readRDS(file='/Users/jennstarling/UTAustin/2016_Fall_SDS 385_Big_Data/Exercise 04 R Code/url_Xt.rds')
+Y_URL <- readRDS(file='/Users/jennstarling/UTAustin/2016_Fall_SDS 385_Big_Data/Exercise 04 R Code/url_y.rds')
+
+#Set up vector of sample sizes.  (All 1 for wdbc data.)
+m_URL <- rep(1,ncol(Xt_URL))
+
+#------------------------------------------------------------------
+#Set up training and test data.
+
+#Split URL data set into 75% train, 25% test.
+train_samps = sample(ncol(Xt_URL)*.75,replace=F)
+
+Xt_URL_tr = Xt_URL[,train_samps]
+Xt_URL_te = Xt_URL[,-train_samps]
+
+Y_URL_tr = Y_URL[train_samps]
+Y_URL_te = Y_URL[-train_samps]
+
+m_URL_tr = m_URL[train_samps]
+m_URL_te = m_URL[-train_samps]
+
+X_URL_te = t(Xt_URL_te)	#Need un-transposed test X for multiplying by beta when predicting.
+
+#------------------------------------------------------------------
+#Run eigen code on small data set, with and without a penalty.
+
+#Lambda=0 (no penalty)
+output_eigen_small <- sparse_sgd_logit(Xt,Y,m,step=5,beta0=rep(0,11),lambda=0,npass=10000)
+output_eigen_small$beta_hat
+
+#Lambda=.1 (with penalty - notice the predictors shrink towards 0)
+output_eigen_small <- sparse_sgd_logit(Xt,Y,m,step=5,beta0=rep(0,11),lambda=.1,npass=10000)
+output_eigen_small$beta_hat
+
+#------------------------------------------------------------------
+#Run eigen code on large sparse URL data set, with and without penalty.
+#Just one pass through data set.
+
+#With lambda=0.
+output_eigen_url <- sparse_sgd_logit(Xt_URL,Y_URL,m_URL,step=1,beta0=rep(0,nrow(Xt_URL)),lambda=0,npass=1)
+head(output_eigen_url$beta_hat)
+range(output_eigen_url$beta_hat)
+length(output_eigen_url$beta_hat)
+
+#With lambda=.1.
+output_eigen_url <- sparse_sgd_logit(Xt_URL,Y_URL,m_URL,step=1,beta0=rep(0,nrow(Xt_URL)),lambda=.1,npass=1)
+head(output_eigen_url$beta_hat)
+range(output_eigen_url$beta_hat)
+length(output_eigen_url$beta_hat)
+
+#Microbenchmark.  Note: was 10.7 seconds before adding lazy recursion term, 13.5 seconds after adding.
+microbenchmark(
+	sparse_sgd_logit(Xt_URL,Y_URL,m_URL,step=1,beta0=rep(0,ncol(Xt_URL)),lambda=0,npass=1),
+	times=1,
+	unit='s'
+)
+
+#------------------------------------------------------------------
+#Test/train prediction:
+
+#Build model on the training data set.  Try lambda=.1
+url_tr <- sparse_sgd_logit(Xt_URL_tr,Y_URL_tr,m_URL_tr,step=1,beta0=rep(0,nrow(Xt_URL)),lambda=.1,npass=1)
+head(url_tr$beta_hat)
+range(url_tr$beta_hat)
+length(url_tr$beta_hat)
+
+#Predicted y values for test set.
+XB = X_URL_te %*% url_tr$beta_hat
+pred_probs = exp(XB) / (1 + exp(XB))
---
+++ bigdata:Exercise 04:stoch_grad_desc_logit_binom_Eigen.cpp
@@ -0,0 +1,227 @@
+#include <RcppEigen.h>
+#include <algorithm>    // std::max
+
+using namespace Rcpp;
+using Eigen::Map;
+using Eigen::MatrixXd;
+using Eigen::LLT;
+using Eigen::Lower;
+using Eigen::MatrixXi;
+using Eigen::Upper;
+using Eigen::VectorXd;
+using Eigen::VectorXi;
+using Eigen::SparseVector;
+typedef Eigen::MappedSparseMatrix<double>  MapMatd;
+typedef Map<MatrixXi>  MapMati;
+typedef Map<VectorXd>  MapVecd;
+typedef Map<VectorXi>  MapVeci;
+
+typedef MapMatd::InnerIterator InIterMat;
+typedef Eigen::SparseVector<double> SpVec;
+typedef SpVec::InnerIterator InIterVec;
+
+template <typename T> int sgn(T val) {
+  return (T(0) < val) - (val < T(0));
+}
+
+//Function to calculate inverse square root.
+//Using this because built-in sqrt function in C++ is very slow.
+//Source:  James' code.
+inline double invSqrt( const double& x ) {
+    double y = x;
+    double xhalf = ( double )0.5 * y;
+    long long i = *( long long* )( &y );
+    i = 0x5fe6ec85e7de30daLL - ( i >> 1 );//LL suffix for (long long) type for GCC
+    y = *( double* )( &i );
+    y = y * ( ( double )1.5 - xhalf * y * y );
+    
+    return y;
+}
+
+//#################################################################
+// [[Rcpp::depends(RcppEigen)]]
+// [[Rcpp::export]]
+
+List sparse_sgd_logit(MapMatd X, VectorXd Y, VectorXd m, double step, 
+					VectorXd beta0, double lambda=0.0, int npass=1){
+  // X is the design matrix stored in column-major format
+  // i.e. with features for case i stores in column i
+  // Y is the vector of counts
+        // M is the vector of sample sizes
+  // Thus Y[i] ~ Binomial( M[i], w[i])  )
+  // w[i] = 1/{1+exp(- x[i] dot beta)}
+  // where Beta is the regression vector we want to estimate
+  // lambda is the regularization parameter
+
+    //Inputs:
+        //X: a nxp design matrix.Design matrix X
+        //Y: a nx1 vector of 1's and 0's; the response vector.
+        //m: a nx1 vector of binomial sample sizes.
+		//discount: The discount to use for the exponential running average for the neg loglhood.
+        //step: the master step size for Adagrad.
+        //maxiter: maximum allowed iterations of algorithm.
+        //tol: tolerance for convergence checking.
+        //samps: a maxiter-length vector specifying X row sampling order.
+        //lambda: the l2 regularization scalar penalty, lambda>0.
+		//npass: the number of passes through the data set to take.
+    //Output: A list containing the following objects.
+        //beta_hat: vector of estimated beta coefficient values.
+        //iter: number of iterations to converge.
+        //loglik: vector of loglikelihood values for each iteration.
+        //loglik_ra: vector of running avg loglikelihood for each iteration.
+    //Notes:
+        //Runs through URL data npass times, in order in file.  No convergence check.
+        //Uses L2 regulariation, with penalty scalar lambda > 0 provided by user.
+        //Uses AdaGrad algorithm.
+	
+	//-----------------------------------
+	//INITIALIZE VALUES:
+	int p = X.rows(); 		//Number of features in X matrix.
+	int n = X.cols(); 		//Number of observations in X matrix.
+	int iter = 0;			//Global iteraton counter.
+	int j = 0;				//Inner iterator row number.
+	double epsilon = 1E-6;	//Constant for Adagrad numeric stability.
+	
+	//Initialize vectors for beta, gradient, and doubles for Adagrad updates in sparse row loop.
+	VectorXd beta_hat(p);			//Beta_hat vector, length p.
+	beta_hat = beta0;				//Set beta_hat to initial beta input value.
+	VectorXd hist_grad(p);		//Vector to hold running hist_grad.  Will be updated piecewise in Sparse Row Loop.
+	VectorXd adj_grad(p);		//Vector to hold adj_grad_j for each beta_hat_j.
+	
+	//Initialize hist_grad values at 1E-3 for numerical stability.
+	for (int i=0;i<p;i++){
+		hist_grad(i) = 1E-3;
+		adj_grad(i) = 0;
+	}
+	
+	double grad_j = 0;				//Holds jth element of gradient.  Do not need to store whole gradient at once.
+	//double adj_grad_j = 0;			//Holds jth element of adj_grad for Adagrad.  (In Sparse Row Loop)
+	
+	//Initialize elements to hold X, Y and m for a single observation (column).
+    SparseVector<double> Xi(n);
+    Xi=X.innerVector(0);
+	
+	double Yi = Y(0);
+	double mi = m(0);
+	double wi = .5;			//wi will be a scalar, since calculating weights in inner vector.
+	double wi_exponent = 0;	//Holds the exponential part of the wi update.
+	
+	//Initialize vector (length p) to keep track of when each predictor updated, for lazy updates.
+	NumericVector last_updated(p,0.0);
+	double skip = 1;	//Holds how many iterations since last update for a j row of ith col.
+	
+	//Initialize vectors to hold log-likelihood and running avg neg log-likelihood.
+	double nll = 0;								//Holds avg neg loglikelihood for the current i obs.	
+	NumericVector nll_ra(n*npass,0.0);			//Stores running avg loglikelihood.
+	
+	//Initialize variable to hold accumulated penalty for a beta_j.
+	double accum_l2_penalty = 0;	//Holds accumulated penalty.
+	double gam = 0;					//Holds step*adj_grad_j, for use in calculating accumulated penalty.
+	
+    //-----------------------------------
+	//LOOPING THROUGH DATA SET:
+	
+    //Loop 1: Loop over entire data set npass times.
+	for (int np=0; np < npass; ++np){
+		
+		//Rcpp::Rcout << "npass:" << np << std::endl; //REMOVE LATER: Output start of each npass through data to R console.
+				
+		//Loop 2:  Loop through observations (cols) of X data set.
+		for (int i=0; i<n; ++i){
+		
+			//Set up the single observation for the next stochastic iteration.  
+			Xi = X.innerVector(i);	//Select new X observation; the ith column of matrix X.
+			Yi = Y(i);				//Select new Y observation; the ith value of vector Y.
+			mi = m(i);				//Select new m observation; the ith value of sample size vector m.			
+		
+			//Update wi probability value.  (w is scalar, since looking at one obs.)
+			wi_exponent = Xi.dot(beta_hat); //breaking out exponential term helps with efficiency.
+			wi = 1 / (1 + exp(-wi_exponent));
+		
+			//Update neg loglikelihood and running average.
+			nll = -(Yi * log(wi + epsilon) + (mi - Yi) * log(1 - wi + epsilon));
+			if(iter > 0) {
+				nll_ra(iter) = (nll_ra(iter-1) * (iter-1) + nll) / iter;
+			}			
+						
+			//Loop 3: Loop through active feature values (rows) of X data set for ith obs (col).
+			for (InIterVec it(Xi); it; ++it){
+				
+				//Set j to the feature (row) number.
+				j = it.index();
+				
+				//--------------------------------
+				//STEP 1: Part 1 of Row Updates for ith Feature:  Deferred (lazy) L2 penalty updates.
+				//This aggregates all of the penalty-only updates since last time a feature was updated.
+				//"Penalty-only" updates refers to the gradient not being updated except
+				//for adding the 2*lambda*beta penalty term.
+				
+				skip = iter - last_updated(j);	//Number of iters since last update. (Skip=1 means updated last iter.)
+				if (skip > 5){  skip = 5;}			//Cap maximum number of recursive updates at 10.
+				last_updated(j) = iter;			//Update the last_updated flag for all j's active in this iter.
+				
+				gam = step*adj_grad(j);
+				
+				//Calculate accum penalty.  Based on recursion defined in my notes.
+				accum_l2_penalty = beta_hat(j) * ( (1 - pow(1-lambda*gam,skip)) / (1 - lambda*gam) );
+				
+				//Add accum l2 penalty to beta_hat_j before doing current iteration update.
+				beta_hat(j) += accum_l2_penalty; 
+			
+				
+				//--------------------------------
+				//STEP 2: Continue with updates for jth row in ith col.
+				
+				//Calculate l2 norm penalty.
+				double l2penalty = 2*skip*lambda*beta_hat(j);
+				
+				//Update the jth gradient term.  Note: it.value() looks up Xji for nonzero entries.
+				grad_j = (mi*wi-Yi) * it.value() + l2penalty;  
+				
+				//Update the jth hist_grad term for Adagrad.  
+				//This is the running total of the jth squared gradient term.
+				hist_grad(j) += grad_j * grad_j;
+				
+				//Calculate the jth adj_grad term for Adagrad.
+				adj_grad(j) = grad_j * invSqrt(hist_grad(j) + epsilon);
+				
+				//Calculate the updated jth beta_hat term.
+				beta_hat(j) -= step*adj_grad(j);	
+			}			
+			++iter; //Update global counter.  (Counts each iteration.)
+		} //End Loop 2: Loop through observations (cols) of X data set.	
+	} //End Loop 1: Loop over entire data set npass times.
+	
+	//-----------------------------------
+	//Done looping, but now must make a final update for applying the penalty for
+	//predictors not updated recently (in last few iterations).
+	//Loop 4: Loop over predictors to 
+	for (int j=0; j<p; ++j){
+		skip = (iter-1) - last_updated(j); 
+			//Using (iter-1) since last_updated indexes from 0, and n is based on counting rows from 1.
+		if (skip > 5){  skip = 5;}			//Cap maximum number of recursive updates at 10.
+		
+		//Calculate accum penalty.
+		gam = step*adj_grad(j);
+		//gam = step*adj_grad_j;	
+		accum_l2_penalty = beta_hat(j) * ( (1 - pow(1-lambda*gam,skip)) / (1 - lambda*gam) );
+		
+		//Update beta_j's to add accum penalty.
+		beta_hat(j) += accum_l2_penalty; 
+
+	}	
+	
+	//-----------------------------------
+	//Return function values.
+    return Rcpp::List::create(
+        _["n"] = n,
+        _["p"] = p,
+		_["iter"] = iter,
+       	_["beta_hat"] = beta_hat,
+		_["loglik"] = nll_ra
+    ) ;
+		
+}
+
+
+
